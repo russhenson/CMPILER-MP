95b6a0c3dc8ef57aaa203ec9bb823af30569bcf8
Russ Henson <henson.russ10@gmail.com>
2021-07-21 13:32:39 +0800

    Create TokenType.java

---
+++ TokenType.java
@@ -0,0 +1,25 @@
+public class TokenType {
+    
+}
+
+//TOKEN TYPE
+    /**
+     * RESERVED - program | begin | end | function | procedure |
+     *            and | or | not | var | const |
+     *            for | to | downto | repeat | until |
+     *            while | do | mod | div | return
+     * 
+     * IDENTIFIERS - [a-zA-Z][a-zA-z0-9]*
+     * 
+     * PREDECLARED - boolean | real | true | read | write |
+     *               char | integer | false | readln | writeln | string
+     * 
+     * COMMA - \,
+     * 
+     * SEMICOLON - \;
+     * 
+     * COLON - \:
+     * 
+     *
+     * 
+     */
