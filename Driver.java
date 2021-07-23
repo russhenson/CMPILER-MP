6cb0973b152819fa47c354b6161f9059859ed13c
GG <gerardstjosol@users.noreply.github.com>
2021-07-23 21:27:52 +0800

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,15 +1,30 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Driver {
-    public static void main(String[] args){
+    public static void main(String[] args) throws FileNotFoundException{
         File inputFile = new File("inputfile.pas");
         File outputFile = new File("outputfile.tok");
         File errorFile = new File("error.txt");
-
         Scanner2 scanner = new Scanner2();
 
+        try(BufferedReader br = new BufferedReader(new FileReader(inputFile))){
+            String line;
+            int lineNum = 0;
+            while((line = br.readLine())!=null){
+                scanner.read_line(inputFile, lineNum);
+                lineNum++;
+            }
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
         
-        scanner.read_line(inputFile, 2);
+        
         String line = scanner.get_line();
         String[] lexemesPerLine = scanner.get_lexeme(line);
 
