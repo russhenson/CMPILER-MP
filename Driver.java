3bda1fbaadc57a7c963ec48e2c0767a1c212f96e
Russ Henson <henson.russ10@gmail.com>
2021-08-02 20:56:48 +0800

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,35 +1,7 @@
 
 
 public class Driver {
-    public static void main(String[] args)/*  throws FileNotFoundException */{
-        /* File inputFile = new File("inputfile.pas");
-        File outputFile = new File("outputfile.tok");
-        File errorFile = new File("error.txt");
-        Scanner2 scanner = new Scanner2();
-
-        try(BufferedReader br = new BufferedReader(new FileReader(inputFile))){
-            String line;
-            int lineNum = 1;
-            while((line = br.readLine())!= null){
-                scanner.read_line(inputFile, lineNum);
-                String oneLine = scanner.get_line();
-                ArrayList<String> lexemesPerLine = scanner.get_lexeme(oneLine);
-                
-                for(int i = 0; i < lexemesPerLine.size(); i++){
-                    System.out.print(scanner.console_dump(oneLine, lexemesPerLine.get(i)));
-                    scanner.file_dump(outputFile, scanner.console_dump(oneLine, lexemesPerLine.get(i)));
-                }
-                
-                lineNum++;
-            }
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
- */
-        
+    public static void main(String[] args) {
         TextEditorGUI editor = new TextEditorGUI();
-        
-
     }
 }
